8dcee3b3492f7f0b02967795da11a2687b0f7d6d
ramyboy98-pixel <ramyboy98@gmail.com>
2026-04-29 11:17:34 +0100

    Update MainActivity.java

--- app/src/main/java/com/aerostock/atelier/MainActivity.java
+++ app/src/main/java/com/aerostock/atelier/MainActivity.java
@@ -3,19 +3,27 @@ package com.aerostock.atelier;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.webkit.WebSettings;
 
 public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        WebView webView = new WebView(this);
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
 
-        webView.loadUrl("file:///android_asset/index.html");
-        setContentView(webView);
-    }
+WebView web = new WebView(this);
+web.setWebViewClient(new WebViewClient());
+
+WebSettings s = web.getSettings();
+s.setJavaScriptEnabled(true);
+s.setDomStorageEnabled(true);
+s.setAllowFileAccess(true);
+s.setAllowContentAccess(true);
+
+web.loadUrl("file:///android_asset/index.html");
+
+setContentView(web);
+}
+
 }
